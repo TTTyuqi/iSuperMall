e51b7e57c5add56d2acee3807c20436fe93b88ba
TTTyuqi <756683214@qq.com>
2020-08-16 22:47:52 +0800

    navrbar和icon的修改

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="nar-bar">
+        <div class="right"><slot name="right"></slot></div>
+        <div class="center"><slot name="center"></slot></div>
+        <div class="left"><slot name="left"></slot></div>
+    </div>
+    </div>  
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'NavBar',
+
+}
+</script>
+<style>
+    .nar-bar{
+        display: flex;
+        height: 44px;
+        text-align: center;
+        align-items: center;
+        box-shadow: 0 1px 1px rgba(50, 50, 50, .2);
+
+    }
+     .left{
+         width: 60px;
+       
+     }
+     .right{
+        width: 60px;
+        
+     }
+     .center{
+         flex: 1;
+     }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>首页</div>
+    <div>
+        <nav-bar class="bagcolor">
+            <div slot="center">商城</div>
+        </nav-bar>
+    </div>
 </template>
 
 <script>
+import NavBar from '@components/common/navbar/NavBar'
     export default {
-        name: "Home"
+        name: "Home",
+        components: {
+            NavBar
+        }
     }
 </script>
 
 <style scoped>
-
+    .bagcolor{
+        background-color: silver;
+        color: ghostwhite;
+    }
 </style>
