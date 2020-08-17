830dbf64d5326a8d14d10f8916abd459e5b8f2d3
TTTyuqi <756683214@qq.com>
2020-08-17 22:15:49 +0800

    特性展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,28 +4,32 @@
             <div slot="center">商城</div>
         </nav-bar>
         <home-swiper :banners="banners"></home-swiper>
+        <recommend-view :recommend="recommend"></recommend-view>
     </div>
 </template>
 
 <script>
 import NavBar from '@components/common/navbar/NavBar'
 import HomeSwiper from './childrenscomponents/HomeSwiper'
+import RecommendView from './childrenscomponents/RecommendView'
 import { getHomeMultiData } from "@/api/home";
 
 export default {
       name: "Home",
       components: {
-        NavBar,HomeSwiper
+        NavBar,HomeSwiper,RecommendView
       },
       data(){
         return{
-          banners:[]
+          banners:[],
+          recommend:[],
         }
       },
       created(){
         getHomeMultiData().then(res => {
           this.banners = res.data.banner.list
-          // console.log('====',this.banners)
+          this.recommend = res.data.recommend.list
+          // console.log('====',res.data)
         })
       }
 
---
+++ src/views/home/childrenscomponents/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="recommend">
+        <div class="recommend-item" v-for="(item,index) in recommend" :key="index">
+            <a :href="item.link" ><img :src="item.image"/></a>
+            <div>{{item.title}}</div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:"RecommendView",
+    props:{
+        recommend:{
+            type:Array,
+            required:true,
+            default:() =>{
+                return []
+            }
+        }
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+    .recommend{
+        display: flex;
+        justify-content: space-around;
+        text-align: center;
+        font-size: 12px;
+        padding: 10px 4px 28px 4px;
+        border-bottom: 10px solid rgba(169, 169, 169,.2);
+    }
+    .recommend-item img{
+        width: 85px;
+        height: 85px;
+        margin-bottom: 8px;
+    }
+</style>
